a7cd86b06324cf73d160586c82f610c081f57157
thangatina1 <montfort1A>
2020-03-24 20:22:52 -0400

    Assignment 2 with SecurityAuth in all apps

--- components/rest-support/src/main/java/io/pivotal/pal/tracker/restsupport/RestConfig.java
+++ components/rest-support/src/main/java/io/pivotal/pal/tracker/restsupport/RestConfig.java
@@ -2,6 +2,7 @@ package io.pivotal.pal.tracker.restsupport;
 
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestOperations;
@@ -11,13 +12,16 @@ import org.springframework.web.client.RestTemplate;
 @Configuration
 public class RestConfig {
 
+    @LoadBalanced
     @Bean
     public RestOperations restOperations() {
+        System.out.println(" ***** I  am here ");
         return new RestTemplate();
     }
 
     @Bean
     public ObjectMapper objectMapper() {
+        System.out.println(" ***** I  am here obj mapper");
         ObjectMapper mapper = new ObjectMapper();
         mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
         return mapper;
