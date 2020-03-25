cd9014c92554c7c7b7402c616e5ffa7bb1ddafb1
thangatina1 <montfort1A>
2020-03-25 00:08:43 -0400

    Assignment 4

--- components/rest-support/src/main/java/io/pivotal/pal/tracker/restsupport/RestConfig.java
+++ components/rest-support/src/main/java/io/pivotal/pal/tracker/restsupport/RestConfig.java
@@ -12,16 +12,15 @@ import org.springframework.web.client.RestTemplate;
 @Configuration
 public class RestConfig {
 
-    @LoadBalanced
-    @Bean
-    public RestOperations restOperations() {
-        System.out.println(" ***** I  am here ");
-        return new RestTemplate();
-    }
+//    @LoadBalanced
+//    @Bean
+//    public RestOperations restOperations() {
+//        System.out.println(" ***** I  am here ");
+//        return new RestTemplate();
+//    }
 
     @Bean
     public ObjectMapper objectMapper() {
-        System.out.println(" ***** I  am here obj mapper");
         ObjectMapper mapper = new ObjectMapper();
         mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
         return mapper;
